95dbe182a6fd36b7bc814cf6f6fe33499083f6fb
ckansiz <canerkansiz@gmail.com>
2015-08-08 11:05:39 +0300

    first

---
+++ home/home.ino
@@ -0,0 +1,96 @@
+#include <SPI.h>
+#include <Ethernet.h>
+#include <dht11.h> // dht11 kütüphanesini ekliyoruz.
+
+#define DHT11PIN 2 // DHT11PIN olarak Dijital 2'yi belirliyoruz.
+
+byte mac[] = {
+  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
+};
+
+IPAddress ip(192, 168, 1, 177);
+EthernetServer server(80);
+dht11 DHT11;
+
+void setup() {
+
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo only
+  }
+
+
+  Ethernet.begin(mac, ip);
+  server.begin();
+  Serial.print("server is at ");
+  Serial.println(Ethernet.localIP());
+}
+
+
+void loop() {
+  // listen for incoming clients
+  EthernetClient client = server.available();
+  if (client) {
+    Serial.println("new client");
+    // an http request ends with a blank line
+    boolean currentLineIsBlank = true;
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        Serial.write(c);
+        // if you've gotten to the end of the line (received a newline
+        // character) and the line is blank, the http request has ended,
+        // so you can send a reply
+        if (c == '\n' && currentLineIsBlank) {
+          // send a standard http response header
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-Type: text/html");
+          client.println("Connection: close");  // the connection will be closed after completion of the response
+          client.println("Refresh: 5");  // refresh the page automatically every 5 sec
+          client.println();
+          client.println("<!DOCTYPE HTML>");
+          client.println("<html>");
+          // output the value of each analog input pin
+          for (int analogChannel = 0; analogChannel < 6; analogChannel++) {
+            int sensorReading = analogRead(analogChannel);
+            client.print("analog input ");
+            client.print(analogChannel);
+            client.print(" is ");
+            client.print(sensorReading);
+            client.println("<br />");
+          }
+          client.println("</html>");
+          break;
+        }
+        if (c == '\n') {
+          // you're starting a new line
+          currentLineIsBlank = true;
+        }
+        else if (c != '\r') {
+          // you've gotten a character on the current line
+          currentLineIsBlank = false;
+        }
+      }
+    }
+    // give the web browser time to receive the data
+    delay(1);
+    // close the connection:
+    client.stop();
+    Serial.println("client disconnected");
+  }
+}
+
+void loop()
+{
+  int chk = DHT11.read(DHT11PIN);
+  if(DHT11.read(DHT11PIN) == 0)
+  {
+    // Sensörden gelen verileri serial monitörde yazdırıyoruz.
+    float _humidity = (float)DHT11.humidity;
+    float _temperature = (float)DHT11.temperature;
+    float _fahrenheit = (float)DHT11.fahrenheit;
+    float _kelvin = (float)DHT11.kelvin
+  }
+}
+
+
